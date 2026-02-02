45983d46002e6e22861a7a6418c4a684550cde6c
Bibhuprasad <bibhuprasadsamal1711@gmail.com>
2026-02-02 21:22:43 +0530

    Day2 components

---
+++ 01_components/src/components/card.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+
+const card = () => {
+    return (
+        <div>
+            <div className='card'>
+                <h1>Bibhuprasad Samal</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium quod explicabo nemo, voluptatum a porro illo blanditiis iure sint impedit officia officiis inventore minus! Corrupti saepe eligendi iure eaque nobis.</p>
+            </div>
+        </div>
+    )
+}
+
+export default card
