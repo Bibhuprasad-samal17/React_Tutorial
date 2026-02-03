1c1b4ab0fcc61771fa4d7cac58b0a162598358c4
Bibhuprasad <bibhuprasadsamal1711@gmail.com>
2026-02-03 20:38:09 +0530

    properties

---
+++ 02_props/src/components/card.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className="parent">
+      <div className="card">
+        <h1>Bibhuprasad Samal</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi necessitatibus debitis rerum mollitia quos vel voluptas obcaecati aperiam expedita provident ad neque nihil sunt, vero corporis culpa quod eligendi. Unde.</p>
+       <button>Click me</button>
+      </div>
+    </div>
+  )
+}
+
+export default Card
