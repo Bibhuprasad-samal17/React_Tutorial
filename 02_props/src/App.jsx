2fa03047b720b9af0ee91a571a13e255c1794dec
Bibhuprasad <bibhuprasadsamal1711@gmail.com>
2026-02-03 23:33:55 +0530

    update

--- 02_props/src/App.jsx
+++ 02_props/src/App.jsx
@@ -4,8 +4,9 @@ import Card from './components/card'
 const App = () => {
   return (
     <div className='parent'>
+      <Card user="John" age = '25' />
+      <Card  user="Jane" age="30" />
       <Card />
-      
     </div>
   )
 }
--- 02_props/src/components/card.jsx
+++ 02_props/src/components/card.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
-const Card = () => {
+const Card = (props) => {
+
+   
   return (
     <div className="parent">
       <div className="card">
-        <h1>Bibhuprasad Samal</h1>
+        <h1>{props.user}</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi necessitatibus debitis rerum mollitia quos vel voluptas obcaecati aperiam expedita provident ad neque nihil sunt, vero corporis culpa quod eligendi. Unde.</p>
        <button>Click me</button>
       </div>
