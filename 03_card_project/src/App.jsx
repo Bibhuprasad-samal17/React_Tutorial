89f7eb7372d27b1e709a7fb48ad85436e8592973
Bibhuprasad <bibhuprasadsamal1711@gmail.com>
2026-02-05 20:16:04 +0530

    asdf

--- 03_card_project/src/App.jsx
+++ 03_card_project/src/App.jsx
@@ -2,15 +2,33 @@ import React from 'react'
 import card from './components/card'
 import user from './components/user'
 
-const arr = [10,20,30,40]
+
 
 const App = () => {
+
+  const arr = [
+    {
+      username: 'john', age: 30
+    },
+    {
+      username: 'jane', age: 25
+    },
+    {
+      username: 'bob', age: 40
+    }]
+
+
+  arr.map(function (elem) {
+    console.log(elem.username);
+  })
+
+
   return (
     <div className='parent'>
-     {arr.map(function(elem){
-      return  <h1>{elem}</h1>
-     })}
-      
+      {arr.map(function (elem) {
+        return <h1>{elem}</h1>
+      })}
+
     </div>
   )
 }
